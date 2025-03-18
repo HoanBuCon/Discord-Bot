475f803f82675b435ea8dbd3faa17355259d9218
HoanBuCon <HoanBuCon>
2025-03-18 23:34:10 +0700

    Update Help-Command

--- src/utils/FileUtils.ts
+++ src/utils/FileUtils.ts
@@ -1,7 +1,15 @@
-import { Message, ChatInputCommandInteraction, MessageComponentInteraction, ModalSubmitInteraction, EmbedBuilder, AttachmentBuilder } from 'discord.js';
+import { Message, ChatInputCommandInteraction, MessageComponentInteraction, ModalSubmitInteraction, EmbedBuilder, AttachmentBuilder, TextChannel, DMChannel, NewsChannel } from 'discord.js';
 import type { Interaction } from 'discord.js';
 import fs from 'fs/promises';
 import path from 'path';
+
+interface MessageOptions {
+    content?: string;
+    embeds?: EmbedBuilder[];
+    components?: any[];
+    files?: AttachmentBuilder[];
+    flags?: number;
+}
 
 export class FileUtils {
     static async readFile(filePath: string): Promise<string | null> {
@@ -32,6 +40,34 @@ export class FileUtils {
             await this.reply(interactionOrMessage, '', embed);
     }
 
+    static async sendMultiFileContent(
+        interactionOrMessage: Message | Interaction,
+        filePaths: string[],
+        customMessage?: string,
+        useEphemeral: boolean = false,
+        firstEmbedReply: boolean = false
+    ): Promise<void> {
+        for (const [index, filePath] of filePaths.entries()) {
+            const content = await this.readFile(filePath);
+            if (!content) {
+                await this.reply(interactionOrMessage, `⚠️ Không thể đọc nội dung file ${filePath}!`);
+                continue;
+            }
+    
+            const embed = this.createEmbed(filePath, content);
+            const isFirstEmbed = index === 0;
+    
+            if (isFirstEmbed && firstEmbedReply) {
+                if (interactionOrMessage instanceof ChatInputCommandInteraction && !interactionOrMessage.deferred && !interactionOrMessage.replied)
+                    await interactionOrMessage.reply({ content: customMessage || '', embeds: [embed], ephemeral: useEphemeral });
+                else
+                    await this.reply(interactionOrMessage, customMessage || '', embed, useEphemeral);
+            } else
+                await this.followUp(interactionOrMessage, '', embed);
+
+        }
+    }
+
     // Phuong thuc gui Media (dung chung cho nhieu lenh, lenh nao khong can dung thi ke me no <("))
     static async sendMedia(
         interactionOrMessage: Message | Interaction,
@@ -42,30 +78,34 @@ export class FileUtils {
         components?: any[]
     ): Promise<Message> {
         const mediaPath = path.resolve(__dirname, mediaDir, mediaFileName);
-    
+
         try {
             await fs.access(mediaPath);
         } catch (error) {
             console.error(`⚠️ File ${mediaFileName} không tồn tại trong thư mục media:`, error);
             throw new Error(`File ${mediaFileName} không tồn tại!`);
         }
-    
-        const attachment = new AttachmentBuilder(mediaPath, { name: mediaFileName });
-        const messageOptions: any = { files: [attachment], };
-    
-        if (content) messageOptions.content = content;
-        if (embed) messageOptions.embeds = [embed];
-        if (components) messageOptions.components = components;
-    
-        if (interactionOrMessage instanceof Message)
-            return await interactionOrMessage.reply(messageOptions);
-        else if (interactionOrMessage instanceof ChatInputCommandInteraction || interactionOrMessage instanceof MessageComponentInteraction || interactionOrMessage instanceof ModalSubmitInteraction) {
-            if (interactionOrMessage.deferred || interactionOrMessage.replied)
-                await interactionOrMessage.followUp(messageOptions);
-            else
-                await interactionOrMessage.reply(messageOptions);
 
-            return await interactionOrMessage.fetchReply();
+        const attachment = new AttachmentBuilder(mediaPath, { name: mediaFileName });
+        const messageOptions: MessageOptions = { files: [attachment] };
+
+        if (content)
+            messageOptions.content = content;
+        if (embed)
+            messageOptions.embeds = [embed];
+        if (components)
+            messageOptions.components = components;
+
+        if (interactionOrMessage instanceof Message) {
+            return await interactionOrMessage.reply(messageOptions);
+        } else if (interactionOrMessage instanceof ChatInputCommandInteraction || interactionOrMessage instanceof MessageComponentInteraction || interactionOrMessage instanceof ModalSubmitInteraction) {
+            if (interactionOrMessage.deferred || interactionOrMessage.replied) {
+                await interactionOrMessage.editReply(messageOptions);
+                return await interactionOrMessage.fetchReply() as Message;
+            } else {
+                await interactionOrMessage.reply(messageOptions);
+                return await interactionOrMessage.fetchReply() as Message;
+            }
         } else
             throw new Error('Loại Interaction này không hỗ trợ reply hoặc fetchReply!');
     }
@@ -187,28 +227,75 @@ export class FileUtils {
     }
 
     private static async reply(
-        interactionOrMessage: Message | Interaction, 
-        message: string, 
-        embed?: EmbedBuilder
+        interactionOrMessage: Message | Interaction,
+        message: string,
+        embed?: EmbedBuilder,
+        useEphemeral: boolean = false
     ): Promise<void> {
         if (interactionOrMessage instanceof Message) {
-            let embedsArray = [];
-            if (embed)
-                embedsArray.push(embed);
-            await interactionOrMessage.reply({ content: message, embeds: embedsArray });
+            let embedsArray: EmbedBuilder[] = [];
+            if (embed) embedsArray.push(embed);
+            await interactionOrMessage.reply({ content: message, embeds: embedsArray }); // Reply cho prefix command
             return;
         }
-    
-        let embedsArray = [];
-        if (embed)
-            embedsArray.push(embed);
-        const options = { content: message, embeds: embedsArray };
-    
+
+        let embedsArray: EmbedBuilder[] = [];
+        if (embed) embedsArray.push(embed);
+        const options: MessageOptions = { content: message, embeds: embedsArray };
+
         if (interactionOrMessage instanceof ChatInputCommandInteraction || interactionOrMessage instanceof MessageComponentInteraction || interactionOrMessage instanceof ModalSubmitInteraction) {
-            if (interactionOrMessage.replied || interactionOrMessage.deferred)
-                await interactionOrMessage.followUp({ ...options, flags: 64 });
+            if (interactionOrMessage.deferred) {
+                const originalReply = await interactionOrMessage.fetchReply();
+                await interactionOrMessage.followUp({
+                    content: originalReply.content + '\n' + (options.content || ''),
+                    embeds: [...originalReply.embeds, ...(options.embeds || [])]
+                });
+            }
+            else if (interactionOrMessage.replied)
+                await interactionOrMessage.followUp(options); // Neu ra reply roi thi gui tiep
             else
-                await interactionOrMessage.reply({ ...options, flags: 64 });
+                await interactionOrMessage.reply(options); // Default
+        }
+    }
+
+    private static async followUp(
+        interactionOrMessage: Message | Interaction,
+        message: string,
+        embed?: EmbedBuilder,
+        useEphemeral: boolean = false
+    ): Promise<void> {
+        if (interactionOrMessage instanceof Message) {
+            let embedsArray: EmbedBuilder[] = [];
+            if (embed) embedsArray.push(embed);
+            const channel = interactionOrMessage.channel;
+
+            if (channel) {
+                if (channel instanceof TextChannel || channel instanceof DMChannel || channel instanceof NewsChannel)
+                    await channel.send({ content: message, embeds: embedsArray });
+                else
+                    throw new Error('Kênh không hỗ trợ gửi tin nhắn!');
+            } else
+                throw new Error('Không tìm thấy kênh để gửi tin nhắn!');
+
+            return;
+        }
+
+        let embedsArray: EmbedBuilder[] = [];
+        if (embed) embedsArray.push(embed);
+        const options: MessageOptions = { content: message, embeds: embedsArray };
+
+        if (interactionOrMessage instanceof ChatInputCommandInteraction || interactionOrMessage instanceof MessageComponentInteraction || interactionOrMessage instanceof ModalSubmitInteraction) {
+            const channel = interactionOrMessage.channel;
+
+            if (!channel)
+                throw new Error('Không tìm thấy kênh để gửi tin nhắn!');
+    
+            if (interactionOrMessage.deferred)
+                await interactionOrMessage.followUp(options);
+            else if (interactionOrMessage.replied)
+                await interactionOrMessage.followUp(options);
+            else
+                await interactionOrMessage.reply(options);
         }
     }
 }
